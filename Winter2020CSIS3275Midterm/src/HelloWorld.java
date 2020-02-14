0fead86d1ee9a5bb97e7a0a83146de79637920df
300304233 <dubeys@student.douglascollege.ca>
2020-02-14 13:38:53 -0800

    Commit

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Saurav Dubey");
+		System.out.print("dubeys@student.douglascollege.ca");
+		
+	}
+
+}
